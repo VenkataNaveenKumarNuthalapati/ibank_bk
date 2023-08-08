3f2403f9874e3c0c050ceaff50031e015a782461
venkatanaveenkumarnuthalapati <venkatanaveenkumarnuthalapati@gmail.com>
2023-08-09 00:42:02 +0530

    ibankDBServer

--- ibank.jsx
+++ ibank.jsx
@@ -3,8 +3,34 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const { getConnection, closeConnection } = require('./init_DB_Server.jsx');
+//////////////////////// SqlServer DB connection /////////////////////////
+const mssql = require('mssql');
 
+const getConnection = async (user, password) => {
+    try {
+        const config = {
+            user: `${user}`, // MySQLServerLogin
+            password: `${password}`, // 4089
+            server: '192.168.157.29',//'DESKTOP-N1JPSN9\\SQLEXPRESS', // 192.168.157.29
+            database: 'IndianBankDB',
+            options: {
+                encrypt: true,
+                trustServerCertificate: true
+            }
+        };
+        await mssql.connect(config);
+        console.log('Connected to SQL Server successfully!');
+        return new mssql.Request();
+    } catch (error) {
+        console.error('Error connecting to SQL Server:', error);
+        throw new Error('Error connecting to the database');
+    }
+};
+
+const closeConnection = () => {
+    mssql.close();
+};
+///////////////////////////////////////////////////////////////////////////
 ////////////////// API 1 /////////////////////////
 app.get('/', async (req, res) => {
     const accNum = req.params.accNum; // Access the parameter using req.params
