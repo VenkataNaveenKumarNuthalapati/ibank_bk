c08bca61b24b984ef1f9c79d9c0a8dac77960598
venkatanaveenkumarnuthalapati <venkatanaveenkumarnuthalapati@gmail.com>
2023-08-08 22:16:09 +0530

    indianBankNaveen

--- ibank.jsx
+++ ibank.jsx
@@ -5,7 +5,22 @@ app.use(cors());
 
 const { getConnection, closeConnection } = require('./init_DB_Server/init_DB_Server.jsx');
 
-////////////////// API 2 /////////////////////////
+////////////////// API 1 /////////////////////////
+app.get('/', async (req, res) => {
+    const accNum = req.params.accNum; // Access the parameter using req.params
+    const request = await getConnection('MySQLServerLogin', '4089');
+    try {
+        const records = await request.query(`SELECT * FROM AccountMaster;`);
+        res.status(200).send(records.recordset);
+    } catch (queryError) {
+        console.error('Error executing SQL query:', queryError);
+        res.status(500).send('Error executing SQL query');
+    } finally {
+        closeConnection();
+    }
+});
+
+////////////////// API 1 /////////////////////////
 app.get('/login/:accNum', async (req, res) => {
     const accNum = req.params.accNum; // Access the parameter using req.params
     const request = await getConnection('MySQLServerLogin', '4089');
